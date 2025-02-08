44216d3303acfa195aa9e0da8b5911041cd9c75c
Abid <abidhussain0495@gmail.com>
2025-02-08 19:49:32 +0530

    Added Committe section

--- src/components/committee.jsx
+++ src/components/committee.jsx
@@ -1,10 +1,48 @@
 export default function Committee() {
     return (
-        <div id="committee" className="flex flex-col mx-4 h-[80vh] border-2 rounded-lg bg-slate-100 my-8 sm:mx-6 lg:mx-20 lg:mt-12 md:p-6 sm:p-2">
-            <h1 className='text-xl font-semibold mx-auto md:text-2xl lg:text-3xl'>Committee</h1>
-            <p className='font-normal text-sm text-justify p-2 text-slate-600 lg:text-base'>
-                The International Conference on “Urban Resilience and Sustainable Architecture” aims to convene distinguished academicians, scientists, professionals, researchers and students to communicate and disseminate their research findings and experiences regarding aspects of Urban Resilience and Sustainable Architecture. It also offers a prominent interdisciplinary forum to present and discuss the most recent innovations, trends, concerns, practical challenges encountered and solutions adopted in Urban Resilience and Sustainable Architecture.
-            </p>
+        <div id="committee" className="flex flex-col gap-4 mx-4 h-full border-2 rounded-lg bg-slate-100 my-8 sm:mx-6 lg:mx-20 lg:mt-12 md:p-6 sm:p-4 p-2">
+            <h1 className='font-semibold mx-auto text-3xl sm:text-5xl'>Committee</h1>
+            <div className="flex gap-4 flex-col">
+                <h1 className="sm:text-3xl text-2xl font-bold">Patron</h1>
+                <div className="text-xl text-wrap">Prof. (Dr.) P. K. Jain, Director, National Institute of Technology Patna</div>
+            </div>
+            <div className="flex gap-4 flex-col">
+                <h1 className="sm:text-3xl text-2xl font-bold">Chairman</h1>
+                <div className="text-xl text-wrap">Prof. (Dr.) Fulena Rajak, Department of Architecture & Planning, National Institute of Technology Patna</div>
+            </div>
+            <div className="flex gap-4 flex-col">
+                <h1 className="sm:text-3xl text-2xl font-bold">Convenor cum Secretary</h1>
+                <div className="text-xl text-wrap">Dr. Ajay Kumar, Associate Professor, Department of Architecture & Planning, National Institute of Technology Patna</div>
+            </div>
+            <div className="flex gap-4 flex-col">
+                <h1 className="sm:text-3xl text-2xl font-bold">Secretaries</h1>
+                <div className="grid gap-4 w-full sm:grid-cols-1 grid-cols-1">
+                    <div className="text-xl ">Dr. B. K. Das, Associate Professor, Department of Architecture & Planning, National Institute of Technology Patna</div>
+                    <div className="text-xl ">Dr. Ravish Kumar, Assistant Professor, Department of Architecture & Planning, National Institute of Technology Patna</div>
+                    <div className="text-xl ">Dr. Kranti Kumar Maurya, Assistant Professor, Department of Architecture & Planning, National Institute of Technology Patna</div>
+                    <div className="text-xl ">Dr. Sandeep Kumar, Assistant Professor, Department of Architecture & Planning, National Institute of Technology Patna</div>
+                    <div className="text-xl ">Ar. Abhishek Kumar Bittu, Assistant Professor, Department of Architecture & Planning, National Institute of Technology Patna</div>
+                    <div className="text-xl ">Ar. Shubham Meena, Assistant Professor, Department of Architecture & Planning, National Institute of Technology Patna</div>
+                    <div className="text-xl ">Ar. Preksha Gupta, Assistant Professor, Department of Architecture & Planning, National Institute of Technology Patna</div>
+                </div>
+            </div>
+            <div className="flex gap-4 flex-col">
+                <h1 className="sm:text-3xl text-2xl font-bold">Organizing Committee</h1>
+                <div className="grid gap-4 w-full sm:grid-cols-3 grid-cols-1">
+                    <div className="text-xl ">Prof. (Dr.) Manoj Kumar, Professor</div>
+                    <div className="text-xl ">Dr. Kamini Sinha, Associate Professor</div>
+                    <div className="text-xl ">Dr. Shailendra K. Mandal, Associate Professor</div>
+                    <div className="text-xl ">Dr. Mazharul Haque, Associate Professor</div>
+                    <div className="text-xl ">Dr. Anjali Sharma, Assistant Professor</div>
+                    <div className="text-xl ">Dr. Md. Danish, Assistant Professor</div>
+                    <div className="text-xl ">Ar. Anushri Barman, Assistant Professor</div>
+                    <div className="text-xl ">Dr. Supriya Kumari, Assistant Professor</div>
+                    <div className="text-xl ">Ar. Rakesh Kumar, Assistant Professor</div>
+                    <div className="text-xl ">Ar. Milind Ashok Kamble, Assistant Professor</div>
+                    <div className="text-xl ">Ar. Pradeep Kr. Kori, Assistant Professor</div>
+                </div>
+            </div>
+            
         </div>
     )
 }
