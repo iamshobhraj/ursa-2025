2c4d045aea952c92a43adc7a86cc10580b7f78fd
iamshobhraj <iamshobhraj@gmail.com>
2025-02-08 16:35:08 +0530

    added mobile nav

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,22 @@
+'use client';
 import Link from "next/link";
 import Nav from "./nav";
 import Image from "next/image";
 import nitp_logo from "../../public/nitp_logo.png";
+import { useState } from "react";
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="fixed h-36 top-0 left-0 right-0 bg-white dark:bg-1/6 z-20">
-        <div className="flex h-[52%] justify-between items-center px-9">
+    <header className="fixed flex flex-row lg:flex-col h-24 lg:h-36 top-0 left-0 right-0 bg-white dark:bg-1/6 z-20">
+        <div className="flex h-full lg:h-[52%] justify-between items-center px-9 w-full">
             <div className="flex flex-col items-center">
                 <h1 className={`antialiased text-4xl tracking-[0.2em] font-[family-name:var(--font-poppins)]`}>U<span className="text-stroke">R</span>SA</h1>
                 <div className="flex w-full justify-between items-center">
-                    {/* <h1 className="font-semibold antialiased text-xs tracking-widest w-full items-center">NIT PATNA</h1> */}
                     <h1 className="font-semibold antialiased text-xs tracking-widest w-full">NOVEMBER 15-16, 2025</h1>
                 </div>
             </div>
@@ -18,12 +25,29 @@ export default function Header() {
                   alt="nitp logo"
                   height="70"
                   width="70"
-                  className="cursor-pointer pt-1"
+                  className="invisible lg:visible cursor-pointer pt-1"
                   href="https://nitp.ac.in/"
                   />
-
+                <button
+                    className="lg:hidden z-50 p-2"
+                    onClick={toggleMenu}
+                >
+                    <div className="space-y-2">
+                        <span className={`block w-8 h-0.5 bg-black transition-transform duration-300 ${isMenuOpen ? 'rotate-45 tranlate-y-2.5' : ''}`}></span>
+                        <span className={`block w-8 h-0.5 bg-black transition-opacity duration-300 ${isMenuOpen ? 'opacity-0' : ''}`}></span>
+                        <span className={`block w-8 h-0.5 bg-black transition-transform duration-300 ${isMenuOpen ? '-rotate-45 tranlate-y-2.5' : ''}`}></span>
+                    </div>
+                </button>
         </div>
-      <Nav />
+        {/* Mobile Navigation Menu */}
+        <div className={`lg:hidden fixed top-32 right-0 h-screen w-64 bg-white dark:bg-gray-800 transform trabsition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+            <Nav isMobile={true} />
+        </div>
+
+        {/* Desktop Navigation*/}
+        <div className="hidden lg:block h-full">
+            <Nav />
+        </div>
     </header>
   );
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,12 +2,20 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 
-export default function Nav() {
+export default function Nav({isMobile}) {
     const [activeHash, setActiveHash] = useState('#home');
+    const navItems = [
+        {label: 'HOME', href: '#home'},
+        {label: 'ABOUT', href: '#about'},
+        {label: 'COMMITTEE', href: '#committee'},
+        {label: 'IMPORTANT DATES', href: '#dates'},
+        {label: 'REGISTRATION FEES', href: '#regfees'},
+        {label: 'GUIDELINES', href: '#guidelines'},
+        {label: 'CALL FOR PAPERS', href: '#call'},
+        {label: 'CONTACT', href: '#contact'}
+    ]
 
     useEffect(() => {
-        const sections = ['home', 'about', 'committee', 'dates', 'regfees', 'guidelines', 'call', 'contact'];
-
         const observerOptions = {
             root: null,
             rootMargin: '-50% 0px',
@@ -24,14 +32,14 @@ export default function Nav() {
 
         const observer = new IntersectionObserver(observerCallback, observerOptions);
 
-        sections.forEach(section => {
-            const element = document.getElementById(section);
+        navItems.forEach(section => {
+            const element = document.getElementById(section.href.split('#')[1]);
             if (element) observer.observe(element);
         });
 
         return () => {
-            sections.forEach(section => {
-                const element = document.getElementById(section);
+            navItems.forEach(section => {
+                const element = document.getElementById(section.href.split('#')[1]);
                 if (element) observer.unobserve(element);
             });
         };
@@ -46,33 +54,13 @@ export default function Nav() {
     }
 
     return (
-        <nav className="flex justify-between h-[48%] items-center font-semibold font-sans w-full text-lg">
-            <div className="flex justify-center items-center h-full w-1/3 border-r border-solid border-[#333333]">X</div>
-            <Link href="#home" onClick={(e) => handleScroll(e, '#home')} className={`flex items-center justify-center h-full w-full ${activeHash === '#home' ? 'bg-[#333333] text-white' : 'hover:bg-[#f0eee8]'}`}>
-                HOME
-            </Link>
-            <Link href="#about" onClick={(e) => handleScroll(e, '#about')} className={`flex items-center justify-center  h-full w-full ${activeHash === '#about' ? 'bg-[#333333] text-white' : 'hover:bg-[#f0eee8]'}`}>
-                ABOUT
-            </Link>
-            <Link href="#committee" onClick={(e) => handleScroll(e, '#committee')} className={`flex items-center justify-center h-full w-full ${activeHash === '#committee' ? 'bg-[#333333] text-white' : 'hover:bg-[#f0eee8]'}`}>
-                COMMITTEE
-            </Link>
-            <Link href="#dates" onClick={(e) => handleScroll(e, '#dates')} className={`flex items-center justify-center h-full w-full ${activeHash === '#dates' ? 'bg-[#333333] text-white' : 'hover:bg-[#f0eee8]'}`}>
-                IMPORTANT DATES
-            </Link>
-            <Link href="#regfees" onClick={(e) => handleScroll(e, '#regfees')} className={`flex items-center justify-center  h-full w-full ${activeHash === '#regfees' ? 'bg-[#333333] text-white' : 'hover:bg-[#f0eee8]'}`}>
-                REGISTRATION
-            </Link>
-            <Link href="#guidelines" onClick={(e) => handleScroll(e, '#guidelines')} className={`flex items-center justify-center  h-full w-full ${activeHash === '#guidelines' ? 'bg-[#333333] text-white' : 'hover:bg-[#f0eee8]'}`}>
-                GUIDELINES
-            </Link>
-            <Link href="#call" onClick={(e) => handleScroll(e, '#call')} className={`flex items-center justify-center  h-full w-full ${activeHash === '#call' ? 'bg-[#333333] text-white' : 'hover:bg-[#f0eee8]'}`}>
-                CALL FOR PAPERS
-            </Link>
-            <Link href="#contact" onClick={(e) => handleScroll(e, '#contact')} className={`flex items-center justify-center  h-full w-full ${activeHash === '#contact' ? 'bg-[#333333] text-white' : 'hover:bg-[#f0eee8]'}`}>
-                CONTACT
-            </Link>
-
+        <nav className={`${isMobile ? 'flex flex-col p-4' : 'flex justify-between items-center font-semibold font-sans w-full text-lg h-full'}`}>
+            <div className="hidden lg:flex justify-center items-center h-full w-1/3 border-r border-solid border-[#333333]">X</div>
+            {navItems.map((item, index) => (
+                <Link key={index} href={item.href} onClick={(e) => handleScroll(e, item.href)} className={`flex items-center justify-center h-full w-full ${activeHash === item.href ? 'bg-[#333333] text-white' : 'hover:bg-[#f0eee8]'}`}>
+                    {item.label}
+                </Link>
+            ))}
         </nav>
     );
 }
