bd3b512966240ee9f11da431cd62160f8d559682
siddhant <sidisan0711@gmail.com>
2025-02-11 21:48:23 +0530

    contact section added

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,9 +1,13 @@
 export default function Contact() {
   return (
-    <div id="contact" className="flex flex-col mx-4 h-[80vh] border-2 rounded-lg bg-slate-100 my-8 sm:mx-6 lg:mx-20 lg:mt-12 md:p-6 sm:p-2 pt-6">
-      <h1 className='text-2xl font-semibold mx-auto lg:text-4xl'>Contact</h1>
-      <p className='font-normal text-sm text-justify p-2 text-slate-600 lg:text-base'>
-        The International Conference on “Urban Resilience and Sustainable Architecture” aims to convene distinguished academicians, scientists, professionals, researchers and students to communicate and disseminate their research findings and experiences regarding aspects of Urban Resilience and Sustainable Architecture. It also offers a prominent interdisciplinary forum to present and discuss the most recent innovations, trends, concerns, practical challenges encountered and solutions adopted in Urban Resilience and Sustainable Architecture.
+    <div id="contact" className="flex flex-col mx-4 border-2 rounded-lg bg-slate-100 my-8 sm:mx-6 lg:mx-20 lg:mt-12 md:p-6 sm:p-2 pt-6">
+      <h1 className='text-2xl font-semibold mx-auto lg:text-4xl'>Contact Us</h1>
+      <p className='font-normal text-sm text-justify p-2 text-slate-600 lg:text-lg md:mt-8'>
+      Department of Architecture and Planning,
+      National Institute of Technology Patna,
+      Ashok Rajpath, Patna 800005, Bihar, India
+      <br/>
+      email: <a className='text-blue-500' href="mailto:ursa2025@nitp.ac.in">ursa2025@nitp.ac.in</a>
       </p>
     </div>
   );
