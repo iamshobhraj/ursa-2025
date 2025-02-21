8e02c93eb1dc7879a4d8a2b3aec45c675f99e7ef
iamshobhraj <iamshobhraj@gmail.com>
2025-02-22 00:11:23 +0530

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import { Geist, Geist_Mono, Poppins, Titan_One } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header";
