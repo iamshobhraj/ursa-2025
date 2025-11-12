4108a435ce4e63a2094dcc617adf4629a12f6d1a
Abid <abidhussain0495@gmail.com>
2025-11-12 22:18:11 +0530

    Added Schedule link

--- src/components/dates.jsx
+++ src/components/dates.jsx
@@ -1,6 +1,6 @@
 export default function Dates() {
     return (
-        <div id="dates" className="flex flex-col gap-16 md:gap-14 mx-4 h-[80vh] border-2 rounded-lg bg-slate-100 my-8 sm:mx-6 lg:mx-20 md:p-6 sm:p-2 px-6 pt-9">
+        <div id="dates" className="flex flex-col gap-8 mx-4 h-[80vh] border-2 rounded-lg bg-slate-100 my-8 sm:mx-6 lg:mx-20 md:p-6 sm:p-2 px-6 pt-9">
             <h1 className='text-2xl font-semibold mx-auto lg:text-4xl'>Important Dates</h1>
             <ul>
                 <li className="bg-slate-300 p-2"><span className="text-lg font-medium">Abstract Submission Deadline: </span><span> October 15, 2025</span></li>
@@ -9,6 +9,20 @@ export default function Dates() {
                 <li className="p-2"><span className="text-lg font-medium">Conference Dates: </span><span>November 15-16, 2025</span></li>
                 <li className="bg-slate-300 p-2"><span className="text-lg font-medium">Full Paper Submission for Book Chapter: </span><span>December 15, 2025</span></li>
             </ul>
+            
+            {/* Updated styling for the bottom div */}
+            <div className="mt-auto w-full p-4 rounded-md text-center">
+                <p className="text-md font-medium">
+                    Looking for the complete timeline?{' '}
+                    <a 
+                        href="https://drive.google.com/file/d/1zscT1FLQVb4iZJxqKp3N5FVuedhXwvQo/view?usp=drive_link" 
+                        target="_blank" 
+                        className="underline underline-offset-4 font-bold"
+                    >
+                        Download Full Schedule
+                    </a>
+                </p>
+            </div>
         </div>
     )
 }
